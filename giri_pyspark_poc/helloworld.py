73ea43f01e0c565aef5be2586ccff862edfbe99c
GiriR Varatharajan <giri.varatharajan@gmail.com>
2016-08-13 16:59:16 -0700

    Basic Changes in the Setup

--- giri_pyspark_poc/helloworld.py
+++ giri_pyspark_poc/helloworld.py
@@ -18,5 +18,6 @@ df.printSchema()
 df2_original = spark.createDataFrame(
     [(50, 100, {"classification": "General", "score": .3493, "fv": "normal"}), (100, 250, {"classification": "outofbox", "score": .3435, "fv": "critical"})], ["num1", "num2", "combined"])
 # df2_original.select("num1","num2",create_map("combined"))
-df2_original.selectExpr("combined","num1","num2").show()
+df2_original.selectExpr("combined","num1","num2").printSchema()
+
 
